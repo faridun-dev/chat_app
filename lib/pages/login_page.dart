c51884e1bf443dbd5312bdae9cecf3d7148a79d5
faridun-dev <faridunf200630@gmail.com>
2024-07-09 16:02:58 +0500

    Initial commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,55 @@
+import 'package:chat_app/components/textfield.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.message_rounded,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Welcome back, you've been missed",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            MyTextField(
+              hintText: "Email",
+              obsecureText: false,
+              controller: emailController,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            MyTextField(
+              hintText: "Password",
+              obsecureText: true,
+              controller: passwordController,
+            ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
